edf6ffa4f5cf0649c4c258fcff2930d0a5dfabd3
jay-lab <forbia3210123@gmail.com>
2024-01-02 01:31:39 +0900

    BottomNavigationBar 구현 - 페이지 전환 기능 적용 - IndexedStack 사용 방식으로 탭의 상태 유지 구현

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -12,7 +12,7 @@ class TabScreen extends StatefulWidget {
 class _TabScreenState extends State<TabScreen> {
   int _selectedPageIndex = 0;
 
-  void _selectPage(int index){
+  void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
     });
@@ -20,12 +20,31 @@ class _TabScreenState extends State<TabScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    // ▼ 1. List<Widget> 사용방식 => 탭의  상태유지 불가
+    // final List<Widget> _pages = [
+    //   const CategoriesScreen(),
+    //   const Placeholder(),
+    // ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Categories'),
         centerTitle: true,
       ),
-      body: const CategoriesScreen(),
+
+      // ▼ 1. List<Widget> 사용방식 => 탭의  상태유지 불가
+      // body: _pages.elementAt(_selectedPageIndex),
+
+      // ▼ 2. IndexedStack 사용방식 => 탭의 상태유지 가능
+      body: IndexedStack(
+        index: _selectedPageIndex,
+        children: const [
+          CategoriesScreen(),
+          Placeholder(),
+          // 다른 페이지 추가...
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
