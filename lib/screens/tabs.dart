0506c5fe03f786b42e3bbfee25ecc499d1cff60d
jay-lab <forbia3210123@gmail.com>
2024-01-01 15:56:39 +0900

    GridView 기틀 구현

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meals_clone/screens/categories.dart';
+import 'package:riverpod/riverpod.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
+
+  @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        centerTitle: true,
+      ),
+      body: CategoriesScreen(),
+    );
+  }
+}
