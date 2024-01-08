951fa5a396775ed8691302fbd0cc57ffe9f907ab
jay-lab <forbia3210123@gmail.com>
2024-01-08 22:55:15 +0900

    2 Depth 상세페이지 구현 - 첫번째 화면으로 가기위해 모든 스택을 초기화하는 방법

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:meals_clone/screens/meal_detail2.dart';
+
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return
-    // const Card(
-    //     child: Center(
-    //       child: Text('Test Page'),
-    //   ),
-    // );
+        // const Card(
+        //     child: Center(
+        //       child: Text('Test Page'),
+        //   ),
+        // );
 
-    // AppBar를 사용하기 위해 Scaffold로 랩핑
-    Scaffold(
+        // AppBar를 사용하기 위해 Scaffold로 랩핑
+        Scaffold(
       appBar: AppBar(
         title: const Text('data'),
       ),
-      body: Card(
-        color: Colors.blueGrey,
-        child: GestureDetector(
-          onTap: (){
-            Navigator.of(context).pop();
-          },
-          child: const Center(
-            child: Text('Test Page'),
+      body: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => const MealDetailsScreen2(),
+            ),
+          );
+          // print('test');
+        },
+        child: Card(
+          color: Colors.blueGrey,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: const Center(
+              child: Text('Test Page'),
+            ),
           ),
         ),
       ),
